58823379dcb9615dfdc00c4db5b711dadc471bb4
Carolina Carranza <00008119@uca.edu.sv>
2020-05-31 01:09:39 -0600

    Add: Recovery exam

---
+++ Reposición/Correccion-preparcial/Preparcial/Modelo/Usuario.cs
@@ -0,0 +1,22 @@
+﻿namespace Preparcial.Modelo
+{
+    public class Usuario
+    {
+        //Corrección: Se eliminaron setters innecesarios
+        public string IdUsuario { get; }
+
+        public string NombreUsuario { get; }
+
+        public string Contrasena { get; }
+
+        public bool Admin { get; }
+
+        public Usuario(string idUsuario, string nombreUsuario, string contrasenia, bool admin)
+        {
+            IdUsuario = idUsuario;
+            NombreUsuario = nombreUsuario;
+            Contrasena = contrasenia;
+            Admin = admin;
+        }
+    }
+}
